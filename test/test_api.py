b79035e5fa6f7f09e5d5450829513f078b2a27bd
sacred70 <sacred70@mail.ru>
2022-12-25 13:34:01 +0700

    финал

--- test/test_api.py
+++ test/test_api.py
@@ -6,21 +6,27 @@ list_keys = ['poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'l
 
 
 def test_json_posts():
-    assert type(json_posts()) == list, print(type(json_posts()))
+    # проверяем возвращается ли список
+    assert type(json_posts()) == list
 
 
 def test_keys_json_posts():
+    # сверяем ключи
     for post in json_posts():
+        # сверяем список ключей поста создавая список через генератор списков
         keys = [key for key in post.keys()]
         assert keys == list_keys
 
 
 def test_post_pk():
+    # проверяем возвращается ли словарь
     for i in range(1, 8):
         assert type(post_pk(i)) == dict
 
 
 def test_keys_post_pk():
+    # сверяем ключи
     for i in range(1, 8):
+        # сверяем список ключей поста создавая список через генератор списков
         keys = [key for key in post_pk(i).keys()]
         assert keys == list_keys
